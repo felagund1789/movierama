f5e0bb7740fb413e4524dd5effed8b5f962b2bf2
Konstantinos K <raistlindragonl@gmail.com>
2024-09-07 02:38:02 +0300

    Add infinite scrolling to the main page

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,28 @@
 import apiClient from "./api-client";
 import { appendMovies } from "./movies";
 
-apiClient.getNowPlaying({ page: 1 }).then((response) => {
-  appendMovies(response.results);
+let isFetching = true;
+let currentPage = 1;
+
+function fetchNowPlaying(page: number): void {
+  apiClient
+    .getNowPlaying({ page })
+    .then((response) => {
+      appendMovies(response.results);
+    })
+    .finally(() => {
+      isFetching = false;
+    });
+}
+
+fetchNowPlaying(currentPage);
+
+window.addEventListener("scroll", async () => {
+  // Do not run if currently fetching
+  if (isFetching) return;
+
+  // Scrolled to bottom
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    fetchNowPlaying(++currentPage);
+  }
 });
