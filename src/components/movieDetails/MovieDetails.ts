4db1e40f20ed29685cc708d0f86f13ce3f357a6c
Konstantinos K <raistlindragonl@gmail.com>
2024-09-12 12:06:14 +0300

    Fix for undefined release year

--- src/components/movieDetails/MovieDetails.ts
+++ src/components/movieDetails/MovieDetails.ts
@@ -117,7 +117,7 @@ export class MovieDetails extends HTMLElement {
           <div class="details-text">
             <h2 class="movie-title">${this.getAttribute("title")}</h2>
             <div class="year-and-score">
-              <h3 class="movie-year">${this.getAttribute("release-date")?.substring(0, 4)}</h3>
+              <h3 class="movie-year">${this.getAttribute("release-date")?.substring(0, 4) || ""}</h3>
               <vote-average average="${this.getAttribute("vote-average")}" />
             </div>
             <div class="movie-genres">${
